72a69c0e3eba6ed3e352322a7a66d52cd4a6eb92
WanM215 <w_m_215@hotmail.com>
2022-04-16 08:21:13 +0700

    changed the datatbase name

--- Includes/Database.php
+++ Includes/Database.php
@@ -2,7 +2,7 @@
 	$host = "localhost";
 	$user = "root";
 	$pass = "";
-	$db = "attendancetestdb";
+	$db = "wanattendance";
 	
 	$conn = new mysqli($host, $user, $pass, $db);
 	if($conn->connect_error){
