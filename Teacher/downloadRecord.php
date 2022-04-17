d18144678e69774d4f821ad36820721c766be960
WanM215 <w_m_215@hotmail.com>
2022-04-17 16:09:58 +0700

    made changes to dowloadRecords.php

--- Teacher/downloadRecord.php
+++ Teacher/downloadRecord.php
@@ -26,7 +26,7 @@ $filename="Attendance list";
 $dateTaken = date("Y-m-d");
 
 $cnt=1;			
-$ret = mysqli_query($conn,"SELECT tblattendance.id,tblattendance.status,tblattendance.date,tblprogramme.programme_name,
+$ret = mysqli_query($conn,"SELECT tblattendance.id,tblattendance.status,tblattendance.dateTimeTaken,tblprogramme.programme_name,
         tblcourse.course_name,tblsemester.semester_name,tblsemester.term_id,tblterm.term_name,
         tblstudents.fullname,tblstudents.username,tblstudents.email,tblstudents.admissionNumber
         FROM tblattendance
@@ -35,7 +35,7 @@ $ret = mysqli_query($conn,"SELECT tblattendance.id,tblattendance.status,tblatten
         INNER JOIN tblsemester ON tblsemester.id = tblattendance.semester_id
         INNER JOIN tblterm ON tblterm.id = tblsemester.term_id
         INNER JOIN tblstudents ON tblstudents.admissionNumber = tblattendance.admissionNo
-        where tblattendance.date = '$dateTaken' and tblattendance.programme_id = '$_SESSION[programme_id]' and tblattendance.course_id = '$_SESSION[course_id]'");
+        where tblattendance.dateTimeTaken = '$dateTaken' and tblattendance.programme_id = '$_SESSION[programme_id]' and tblattendance.course_id = '$_SESSION[course_id]'");
 
 if(mysqli_num_rows($ret) > 0 )
 {
@@ -56,7 +56,7 @@ echo '
 <td>'.$semester_name=$row['semester_name'].'</td>	 
 <td>'.$term_name=$row['term_name'].'</td>	
 <td>'.$status=$status.'</td>	 	
-<td>'.$date=$row['date'].'</td>	 					
+<td>'.$date=$row['dateTimeTaken'].'</td>	 					
 </tr>  
 ';
 header("Content-type: application/octet-stream");
